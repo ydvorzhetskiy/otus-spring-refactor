16250506cc995d1240098ea6cfb0c754eae06f06
ydvorzhetskiy <ydvorzhetskiy@navicons.ru>
2020-08-12 11:24:46 +0300

    Initial commit

---
+++ src/main/java/ru/otus/springrefactor/SpringRefactorApplication.java
@@ -0,0 +1,13 @@
+package ru.otus.springrefactor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRefactorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRefactorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/otus/springrefactor/SpringRefactorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.otus.springrefactor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRefactorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
